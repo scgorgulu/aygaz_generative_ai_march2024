f57d9172bd06bcb9f6e45b765ad09797c4975da3
Serhat Cagri Gorgulu <scgorgulu@gmail.com>
2024-03-23 15:00:16 +0300

    2nd Attempt

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Example: Show an alert when the contact section is clicked
+    $('#contact').click(function() {
+        alert('Contact form will be here!');
+    });
+});
